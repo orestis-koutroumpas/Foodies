250646bccf30ef3f94fa9d4818b828ece463c0a2
Orestis Koutroumpas <o.koutroubas@gmail.com>
2024-05-01 19:22:02 +0300

    connect to db

--- model/model.mjs
+++ model/model.mjs
@@ -1,1 +1,20 @@
 import pg from 'pg';
+import dotenv from 'dotenv';
+
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+         rejectUnauthorized: false 
+    }
+});
+
+async function connect() {
+    try {
+        const client = await pool.connect();
+        return client;
+    }
+    catch(e) {
+        console.error(`Failed to connect ${e}`);
+    }
+}
+
