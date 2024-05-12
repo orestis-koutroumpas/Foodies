a9d58e8ec1d2baea5736d7d7052f14baa48372ff
Orestis Koutroumpas <o.koutroubas@gmail.com>
2024-05-12 19:31:53 +0300

    data for home page from db

--- model/db/foodies_db.sql
+++ model/db/foodies_db.sql
@@ -83,16 +83,13 @@ CREATE TABLE IF NOT EXISTS "store_menu" (
 
 INSERT INTO "store" ("id", "name", "address", "description", "category", "rating", "min_order", "delivery_fee", "estimated_delivery_time", "delivery_times", "phone_number")
 VALUES 
-(1, 'Burger Park', '1234 Maple Avenue', 'Juicy burgers in a cozy atmosphere!', 'Burger', 4, 10, 1, '00:30:00', 'Mon-Sat: 10:00 AM - 10:00 PM, Sun: 12:00 PM - 9:00 PM', '555-9876'),
-(2, 'Coffee Lab', '789 Elm Street', 'Artisan coffee blends for every palate!', 'Coffee', 3, 8, 2, '00:45:00', 'Mon-Fri: 11:00 AM - 9:00 PM', '555-5432'),
-(3, 'Crepes Go', '456 Oak Avenue', 'Savory and sweet crepes made to order!', 'Crepes', 3, 8, 2, '00:45:00', 'Mon-Fri: 11:00 AM - 9:00 PM', '555-2468'),
-(4, 'Fresh', '789 Pine Avenue', 'Indulge in our decadent desserts!', 'Pancakes', 3, 8, 2, '00:45:00', 'Mon-Fri: 11:00 AM - 9:00 PM', '555-1357'),
-(5, 'Hendersons', '567 Cedar Street', 'Satisfy your hunger with our gourmet sandwiches!', 'Sandwich', 3, 8, 2, '00:45:00', 'Mon-Fri: 11:00 AM - 9:00 PM', '555-8023'),
-(6, 'Mr Coffee', '789 Maple Avenue', 'Experience the aroma of freshly brewed coffee!', 'Coffee', 3, 8, 2, '00:45:00', 'Mon-Fri: 11:00 AM - 9:00 PM', '555-6789'),
-(7, 'Pizzeria', '345 Elm Street', 'Authentic Italian pizzas made with love!', 'Pizza', 3, 8, 2, '00:45:00', 'Mon-Fri: 11:00 AM - 9:00 PM', '555-4321'),
-(8, 'Burger Premium', '123 Main Street', 'Tasty burgers made fresh!', 'Burger', 4, 10, 1, '00:30:00', 'Mon-Sun: 11:00 AM - 10:00 PM', '555-1234'),
-(9, 'Fast Pizza', '456 Oak Avenue', 'Delicious pizzas served hot!', 'Pizza', 4, 25, 0.5, '00:40:00', 'Mon-Sat: 11:30 AM - 10:00 PM', '555-5678'),
-(10, 'Pancakes Pantry', '789 Maple Drive', 'Hearty breakfasts to start your day right!', 'Pancakes', 3, 15, 2, '00:25:00', 'Mon-Sun: 7:00 AM - 12:00 PM', '555-9012'),
-(11, 'Pasta Pulse', '1010 Pine Lane', 'Satisfying pasta dishes for every palate!', 'Pasta', 4, 5, 0, '00:35:00', 'Mon-Sat: 11:00 AM - 9:00 PM', '555-3456'),
-(12, 'Sandwich Spot', '1212 Cedar Street', 'Fresh wraps bursting with flavor!', 'Sandwich', 2, 5, 0.5, '00:30:00', 'Mon-Fri: 10:30 AM - 8:00 PM', '555-6789'),
-(13, 'Sushi Sail', '1414 Spruce Avenue', 'Exquisite sashimi prepared with care!', 'Sushi', 5, 5, 0.5, '00:50:00', 'Tue-Sun: 12:00 PM - 9:00 PM', '555-7890');
+(1, 'Burger Park', 'Riga Feraiou 30', 'Juicy burgers in a cozy atmosphere!', 'Burger', 4, 10, 1, '00:30:00', 'Mon-Sat: 10:00 AM - 10:00 PM, Sun: 12:00 PM - 9:00 PM', '555-9876'),
+(2, 'Burger Premium', 'Lontou 150', 'Tasty burgers made fresh!', 'Burger', 4, 10, 1, '00:30:00', 'Mon-Sun: 11:00 AM - 10:00 PM', '555-1234'),
+(3, 'Coffee Lab', 'Karolou 40', 'Artisan coffee blends for every palate!', 'Coffee', 3, 8, 2, '00:45:00', 'Mon-Fri: 11:00 AM - 9:00 PM', '555-5432'),
+(4, 'Mr Coffee', 'Papaflessa 52', 'Experience the aroma of freshly brewed coffee!', 'Coffee', 3, 8, 2, '00:45:00', 'Mon-Fri: 11:00 AM - 9:00 PM', '555-6789'),
+(5, 'Crepes Go', 'Astiggos 9', 'Savory and sweet crepes made to order!', 'Crepes', 3, 8, 2, '00:45:00', 'Mon-Fri: 11:00 AM - 9:00 PM', '555-2468'),
+(6, 'Hendersons', 'Zaimi 93', 'Satisfy your hunger with our gourmet sandwiches!', 'Sandwich', 3, 8, 2, '00:45:00', 'Mon-Fri: 11:00 AM - 9:00 PM', '555-8023'),
+(7, 'Sandwich Spot', 'Solwmou 11', 'Fresh wraps bursting with flavor!', 'Sandwich', 2, 5, 0.5, '00:30:00', 'Mon-Fri: 10:30 AM - 8:00 PM', '555-6789'),
+(8, 'Pizzeria', 'Smyrnis 61', 'Authentic Italian pizzas made with love!', 'Pizza', 3, 8, 2, '00:45:00', 'Mon-Fri: 11:00 AM - 9:00 PM', '555-4321'),
+(9, 'Fast Pizza', 'Danihlidos 17', 'Delicious pizzas served hot!', 'Pizza', 4, 25, 0.5, '00:40:00', 'Mon-Sat: 11:30 AM - 10:00 PM', '555-5678'),
+(10, 'Pasta Pulse', 'Maizwnos 315', 'Satisfying pasta dishes for every palate!', 'Pasta', 4, 5, 0, '00:35:00', 'Mon-Sat: 11:00 AM - 9:00 PM', '555-3456')
--- model/model.mjs
+++ model/model.mjs
@@ -1,15 +1,13 @@
 // model/model.mjs
 
-import fs from 'fs';
-import path from 'path';
+import db from 'better-sqlite3'
+const sql = new db('model/db/foodies.sqlite', { fileMustExist: true });
 
-// Function to read data from stores_data.json
 export const getAllStores = () => {
     try {
-        const data = fs.readFileSync('C:/Users/okout/Programming Files/Internet Programming Course 2023-2024/Foodies/model/stores_data.json', 'utf-8');
-        return JSON.parse(data);
+        const stmt = sql.prepare("SELECT * FROM store");
+        return stmt.all();
     } catch (error) {
-        console.error('Error reading stores_data.json:', error);
-        return null;
+        throw error;
     }
 };
